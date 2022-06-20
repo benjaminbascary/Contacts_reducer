6de5f12ee518f3a4e16410374d26424841389a4f
Benjamin Bascary <benjaminbascary@gmail.com>
2022-06-20 18:43:11 -0300

    Commits final changes to index.html and manifest.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import Header from "./components/Header";
 import ContactsTable from "./components/ContactsTable";
-import { NewUserForm } from "./components/NewUserForm";
-import { useState } from "react";
 
 function App() {
-
-  const [isActive, setIsActive] = useState<boolean>(false);
 
   return (
     <>
